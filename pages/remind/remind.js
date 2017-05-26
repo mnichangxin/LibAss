c6279d2f46441a17c5298689c2ba23be60bc288d
mnichangxin <mnichangxin@163.com>
2017-05-27 00:44:14 +0800

    commit

--- pages/remind/remind.js
+++ pages/remind/remind.js
@@ -1,66 +1,24 @@
 // remind.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
+  // 页面初始数据
   data: {
-  
+    book: {
+      id: '',
+      book_title: '百年孤独',
+      holding: 10
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  // 事件处理函数
   onLoad: function (options) {
-  
+    this.data.book.id = options.id;
+    this.setData({
+      book: this.data.book
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
   
-  },
+  subscribeTap: function() {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
   }
 })
