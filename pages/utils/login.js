5e5becaeaf0878a6fc7348e60d88163fb97cc9dc
mnichangxin <mnichangxin@163.com>
2017-05-28 22:49:14 +0800

    commit

---
+++ pages/utils/login.js
@@ -0,0 +1,24 @@
+/* 登录组件 */
+function login() {
+  wx.login({
+    success: function (res) {
+      wx.request({
+        url: 'https://85293008.qcloud.la/weiapp/login',
+        data: {
+          code: res.code
+        },
+        success: function (res) {
+          for (var k in res.data) {
+            wx.setStorage({
+              key: k,
+              data: res.data[k],
+            });
+          }
+          console.log(res.data);
+        }
+      });
+    }
+  });
+}
+
+module.exports.login = login;
