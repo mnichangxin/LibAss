68e7feca7dfd995335c36c07033da8b0b663d42d
mnichangxin <mnichangxin@163.com>
2017-05-29 16:41:39 +0800

    commit

--- pages/utils/login.js
+++ pages/utils/login.js
@@ -3,7 +3,7 @@ function login() {
   wx.login({
     success: function (res) {
       wx.request({
-        url: 'https://85293008.qcloud.la/wxapp/soft/loginWeChatCheck.action',
+        url: 'https://85293008.qcloud.la/wxapp/soft/loginWeChatCheck.action', // 请求登录的地址
         data: {
           code: res.code
         },
@@ -12,10 +12,16 @@ function login() {
             key: 'token',
             data: res.data.token
           });
-          console.log('token:' + res.data.token);
+          console.log('登陆成功，token为：' + res.data.token);
+        },
+        fail: function() {
+          console.log('登录失败');
         }
       });
       console.log('code:' + res.code);
+    },
+    fail: function() {
+      console.log('调用登录接口失败');
     }
   });
 }
